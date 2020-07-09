d6b9e95091af992e680ff3d6352b1094c24de8b4
darshansivaram07 <54503110+darshansivaram07@users.noreply.github.com>
2020-07-09 17:22:45 +0530

    Finished

--- products/urls.py
+++ products/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path,include
-from products import views
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('accounts/',include('accounts.urls')),
-    path('products/',include('products.urls')),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('create',views.create,name='create'),
+    path('<int:product_id>',views.details,name='details'),
+     path('<int:product_id>/upvote', views.upvote, name='upvote'),
+    
+]
+
--- products/views.py
+++ products/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
   return render(request,'products/home.html')
+
+@login_required(login_url='/accounts/signup')
+def create(request):
+  if request.method== 'POST':
+    if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
+      product = Product()
+      product.title=request.POST['title']
+      product.body=request.POST['body']
+      product.image=request.FILES['image'] 
+      product.icon=request.FILES['icon'] 
+      if request.POST['url'].startswith('https://') or request.POST['url'].startswith('http://'):
+        product.url=request.POST['url']
+      else:
+        product.url= 'http://'+ request.POST['url']
+      product.pub_date=timezone.datetime.now()
+      product.hunter=request.user
+      product.save()
+      return redirect('/products/'+ str(product.id))
+    else:
+      return render(request,'products/create.html',{'error':'Enter all fiel'})
+      
+
+  else:
+    return render(request,'products/create.html')
+
+
+def details(request,product_id):
+  product=get_object_or_404(Product,pk=product_id)
+  return render(request,'products\details.html',{'product':product})
+@login_required(login_url='/accounts/signup')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
+
+def home(request):
+    products = Product.objects
+    return render(request, 'products/home.html',{'products':products})
